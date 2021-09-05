4b5f6fd524f63109699c8c070c16750ab3704ca2
Vehementz <vale-tudo91@sfr.fr>
2021-09-05 13:20:55 +0200

    quantity, name, price, total price and total articles implemented in javascript and html

--- cart.js
+++ cart.js
@@ -1,4 +1,7 @@
 let productSaveInLocalStorage = JSON.parse(localStorage.getItem("product"));
+let totalPriceInLocalStorage = parseFloat(JSON.parse(localStorage.getItem("total-cost")));
+let totalArticlesInLocalStorage = Number(JSON.parse(localStorage.getItem("nbArticlesTotal")));
+
 let cartContainer = document.querySelector("#cart-container");
 
 
@@ -14,15 +17,42 @@ else {
     let productCartStructure = [];
 
     for (j = 0; j < productSaveInLocalStorage.length; j++) {
+        productSaveInLocalStorage[j] 
 
-        let cardContainer = document.createElement("div");
-        let card = document.createElement("div");
-        let cardImg = document.createElement("img");
-        let cardDescription = document.createElement("div");
-        let productName = document.createElement("p");
-        let quantity = document.createElement("p");
+        let nameElement = document.createElement("p");
+        let productName = document.querySelector("#name-category");
+        nameElement.innerHTML = productSaveInLocalStorage[j].name;
+        productName.appendChild(nameElement);
 
+        let lensElement = document.createElement("p");
+        let productLens = document.querySelector("#lens-category");
+
+        lensElement.innerHTML = productSaveInLocalStorage[j].options;
+        productLens.appendChild(lensElement);
+
+        let priceElement = document.createElement("p");
+        let productPrice = document.querySelector("#price-category");
+        priceElement.innerHTML = `${(productSaveInLocalStorage[j].price) *(productSaveInLocalStorage[j].quantity)} €`;
+        productPrice.appendChild(priceElement);
+
+        let quantityElement = document.createElement("p");
+        let productQuantity = document.querySelector("#quantity-category");
+        quantityElement.innerHTML = productSaveInLocalStorage[j].quantity;
+        productQuantity.appendChild(quantityElement);
     }
 
-    
+    let totalPriceP = document.createElement("p");
+        let totalPrice = document.querySelector("#total-price");
+        totalPriceP.innerHTML = `${totalPriceInLocalStorage} €`;
+        totalPrice.appendChild(totalPriceP);
+
+     let nbTotalArticlesP = document.createElement("p");
+     let nbTotalArticles = document.querySelector("#nb-total-articles");
+     nbTotalArticlesP.innerHTML = totalArticlesInLocalStorage;
+     nbTotalArticles.appendChild(nbTotalArticlesP);
+
 } 
+
+/* ---------------------- Vider le panier -----------------*/
+
+const clearAllProduct = document.querySelector("#clear-whole-cart");
