bf8d56dfc1201f57634a639c551c6094099cabaa
Vehementz <vale-tudo91@sfr.fr>
2023-11-02 19:39:00 +0100

    ux improved

--- app.js
+++ app.js
@@ -25,43 +25,48 @@ fetch(urlApi)
 
 
     for(let i = 0; i < data.length; i++) {
+  // Create the anchor tag
+          let detailLink = document.createElement("a");
+          detailLink.href = "./product.html?id=" + data[i]._id;
+          detailLink.classList.add("camera-card", "detail-link");
+          
+          // Create the card container
+          let carte = document.createElement("div");
+          carte.classList.add("camera-card-content");
 
-      let carte = document.createElement("div");
-      let imgCard = document.createElement("img");
-      imgCard.src = data[i].imageUrl;
-      let containerDescription = document.createElement("div");
-      containerDescription.classList.add("container-description");
+          // Image for the card
+          let imgCard = document.createElement("img");
+          imgCard.src = data[i].imageUrl;
 
-      let name = document.createElement("p");
-      name.innerText = data[i].name;
-      name.classList.add("contenair-description-name");
+          // Container for the description
+          let containerDescription = document.createElement("div");
+          containerDescription.classList.add("container-description");
+
+          // Name of the camera
+          let name = document.createElement("p");
+          name.innerText = data[i].name;
+          name.classList.add("container-description-name");
+
+          // Price of the camera
+          let price = document.createElement("p");
+          let priceInit = Number(data[i].price) / 100;
+          let priceConv = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(priceInit);
+          price.innerText = priceConv;
+          price.classList.add("container-description-price");
+
+          // Append all the card content to the card container
+          carte.appendChild(imgCard);
+          carte.appendChild(containerDescription);
+          containerDescription.appendChild(name);
+          containerDescription.appendChild(price);
+
+          // Now, wrap the card content with the detail link
+          detailLink.appendChild(carte);
+          
+          // Append the detail link (which now contains the card) to the main container
+          cameraContainer.appendChild(detailLink);
       
-      let price = document.createElement("p");
-      // price.innerText = data[i].price;
-      let priceInit = Number(data[i].price) / 100;
-      priceConv = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(priceInit);
-      price.innerText = priceConv;
       
-      price.classList.add("container-description-price");
-
-
-  
-      
-      cameraContainer.appendChild(carte);
-  
-      carte.classList.add("camera-card");
-      carte.appendChild(imgCard);
-      
-      carte.appendChild(containerDescription);
-      containerDescription.appendChild(name);
-      containerDescription.appendChild(price);
-
-
-      let detailLink = document.createElement("a");
-      containerDescription.appendChild(detailLink);
-      detailLink.innerText = "Details";
-      detailLink.href = "./product.html?id=" + data[i]._id;
-      detailLink.classList.add("detail-link");
       
 }
 
